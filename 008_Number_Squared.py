f601ed2e7427203329865a6131bb2e6da5712813
Karan Salunkhe <karansalunkhe180@gmail.com>
2025-04-21 09:57:48 +0530

    Create 008_Number_Squared.py

---
+++ 008_Number_Squared.py
@@ -0,0 +1,5 @@
+// Write a program that asks the user for a number and outputs the number squared that is enter
+
+num = int(input("Enter the number: "))
+sq = num * num
+print("The square is:", sq)
